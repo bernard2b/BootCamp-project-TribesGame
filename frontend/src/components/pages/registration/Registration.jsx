79b43c271397cd8e1dffe0e64f02286ef5d9d262
ImJPeti <jovanovicsp@gmail.com>
2022-12-13 16:29:05 +0100

    Formating the reg form

--- frontend/src/components/pages/registration/Registration.jsx
+++ frontend/src/components/pages/registration/Registration.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-
+import Header from "../../header/Header";
+import "./Registration.scss";
 
 export default function Registration() {
   
   return(
-    <div>
-        registration
+    <div className="Registration">
+      <Header />
+      <h1>Tribes of Strigops</h1>
+      <div className="registerForm">
+        <form>
+          <input type="text" name="username" id="username" placeholder="Username" />
+          <input type="password" name="password" id="password" placeholder="Password" />
+          <input type="text" name="nameOfKingdom" id="kingdomName" placeholder="Kingdom name" />
+          <input type="submit" value="SIGN UP"/>
+        </form>
+      </div>
     </div>
   )
 }
