5df27768e7657dc7824183d276f500cf89123a5a
ImJPeti <jovanovicsp@gmail.com>
2022-12-13 15:20:45 +0100

    Add /Register endpoint

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './landing/Landing';
+import Registration from './pages/registration/Registration';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<Registration />} />
         <Route path="*" element={<Landing />} />
       </Routes>
     </BrowserRouter>
