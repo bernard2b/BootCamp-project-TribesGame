5c3c668af35b835092344415ae00366134819320
bernard.gyugyi <bernard.gyugyi@gmail.com>
2022-12-13 15:08:15 +0100

    replace links to images

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -6,10 +6,10 @@ function Menu() {
   return (
     <div className='Menu'>
       <div className="section">
-          <div className="menuComponent"><Link to='/kingdom/buildings'>Buildings</Link></div>
-          <div className="menuComponent"><Link to='/kingdom/battle'>Battle</Link></div>
-          <div className="menuComponent"><Link to='/kingdom/leaderboards'>Leaderboard</Link></div>
-          <div className="menuComponent"><Link to='/kingdom/troops'>Troops</Link></div>
+          <div className="menuComponent"><Link to='/kingdom/buildings'><img src={require('./menu-pictures/buildings.jpg')} /></Link></div>
+          <div className="menuComponent"><Link to='/kingdom/battle'><img src={require('./menu-pictures/attack.jpg')} /></Link></div>
+          <div className="menuComponent"><Link to='/kingdom/leaderboard'><img src={require('./menu-pictures/leaderboard.jpg')} /></Link></div>
+          <div className="menuComponent"><Link to='/kingdom/troops'><img src={require('./menu-pictures/troops.jpg')} /></Link></div>
         </div>
     </div>
   )
