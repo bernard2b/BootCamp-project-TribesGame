c8028f8b1ca912c8c1490f5ae1c5fd1e6ddf980e
ImJPeti <jovanovicsp@gmail.com>
2022-12-13 16:44:42 +0100

    Done with the reg frontend /register

--- frontend/src/components/pages/registration/Registration.jsx
+++ frontend/src/components/pages/registration/Registration.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../../header/Header";
 import "./Registration.scss";
 
 export default function Registration() {
-  
+
   return(
     <div className="Registration">
       <Header />
       <h1>Tribes of Strigops</h1>
       <div className="registerForm">
         <form>
-          <input type="text" name="username" id="username" placeholder="Username" />
-          <input type="password" name="password" id="password" placeholder="Password" />
-          <input type="text" name="nameOfKingdom" id="kingdomName" placeholder="Kingdom name" />
-          <input type="submit" value="SIGN UP"/>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            required
+          />
+          
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            minLength={8}
+            required
+          />
+          
+          <input
+            type="text"
+            name="nameOfKingdom"
+            id="kingdomName"
+            placeholder="Kingdom name"
+            required
+          />
+
+          <input
+            type="submit"
+            value="SIGN UP"
+          />
         </form>
       </div>
     </div>
