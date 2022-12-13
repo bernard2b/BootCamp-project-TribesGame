bf2bf4a33bd0804c6d1e8ef70e7ec7c6eb4ee64a
maakhu <egri@me.com>
2022-12-13 10:21:14 +0100

    add react router base for 'not developed yet' 404 page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 root.render(
   <React.StrictMode>
+        <RouterProvider router={router} />
+
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
