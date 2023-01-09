4c15890a78d8e94cb119498baf12f6136e8c55a9
maakhu <egri@me.com>
2023-01-09 15:15:39 +0100

    not found page text update to scifi

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -4,14 +4,16 @@ export default function NotFound() {
   return (
     <div>
       <h1>Stop your spaceship, traveller!</h1>
+      <br></br>
       <h3>
         This space station is under construction. You might want to come back
         later, or travel in time to its completion.
       </h3>
-      <p>
+      <h3>
         Until then, please follow our patrol ships back to the closest space
         station:
-      </p>
+      </h3>
+      <br></br>
       <Link to="/">Back to the galaxy</Link>
     </div>
   );
