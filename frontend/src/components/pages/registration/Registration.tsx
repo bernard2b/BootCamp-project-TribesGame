58822ba82ccee9fcc132639b74ed9536138dca62
ImJPeti <potyipeti@gmail.com>
2023-01-10 15:26:57 +0100

    DOne w frontend validation and ugfy af frontend

--- frontend/src/components/pages/registration/Registration.tsx
+++ frontend/src/components/pages/registration/Registration.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../header/Header";
 import "./Registration.scss";
 import Avatar from '@mui/material/Avatar';
@@ -13,106 +13,148 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme();
-
-
-
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { literal, object, string, TypeOf } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function Registration() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const theme = createTheme();
+
+  const registerSchema = object({
+    firstName: string()
+      .nonempty('Name is required'),
+    lastName: string()
+      .nonempty("Name is required"),
+    email: string().nonempty('Email is required').email('Email is invalid'),
+    password: string()
+      .nonempty('Password is required')
+      .min(8, 'Password must be more than 8 characters')
+      .max(32, 'Password must be less than 32 characters'),
+    passwordConfirm: string().nonempty('Please confirm your password'),
+    terms: literal(true, {
+      invalid_type_error: 'Accept Terms is required',
+    }),
+  }).refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: 'Passwords do not match',
+  });
+
+  type RegisterInput = TypeOf<typeof registerSchema>;
+
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmitHandler: SubmitHandler<RegisterInput> = (values) => {
+    console.log(values);
   };
 
-  return(
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  return (
     <div className="Registration">
       <Header />
-        <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: "#f8f9fa",
-            padding: 2,
-            borderRadius: 1,
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          </Avatar>
-            <h1 style={{color: "black"}}>SIGN UP</h1>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              backgroundColor: "#f8f9fa",
+              padding: 2,
+              borderRadius: 1,
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            </Avatar>
+            <h1 style={{ color: "black" }}>SIGN UP</h1>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(onSubmitHandler)} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    error={!!errors["firstName"]}
+                    helperText={errors["firstName"] ? errors["firstName"].message : ''}
+                    {...register("firstName")}
+                    autoFocus
+                  />
                 </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    autoComplete="family-name"
+                    error={!!errors["lastName"]}
+                    helperText={errors["lastName"] ? errors["lastName"].message : ''}
+                    {...register("lastName")}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    autoComplete="email"
+                    error={!!errors["email"]}
+                    helperText={errors["email"] ? errors["email"].message : ''}
+                    {...register("email")}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    error={!!errors["password"]}
+                    helperText={errors["password"] ? errors["password"].message : ""}
+                    {...register("password")}
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-      </div>
+        </Container>
+      </ThemeProvider>
+    </div>
   )
 }
