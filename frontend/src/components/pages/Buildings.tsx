583b30dcde6bae95d51be1f130142f14fd5f49d8
ImJPeti <potyipeti@gmail.com>
2023-01-17 16:30:16 +0100

    buildings in their correct place

--- frontend/src/components/pages/Buildings.tsx
+++ frontend/src/components/pages/Buildings.tsx
@@ -5,33 +5,6 @@ import fetchBuildings from "../../api/buildings";
 import buildingsInterface from "./../../interfaces/buildingsInterface";
 
 
-const buildingsData = [
-  {
-    id: "1",
-    title: "Townhall",
-    level: "1",
-    img: require("./buildings-pictures/townhall.png"),
-  },
-  {
-    id: "2",
-    title: "Academy",
-    level: "1",
-    img: require("./buildings-pictures/addacademy.png"),
-  },
-  {
-    id: "3",
-    title: "Farm",
-    level: "1",
-    img: require("./buildings-pictures/farm.png"),
-  },
-  {
-    id: "4",
-    title: "Mine",
-    level: "1",
-    img: require("./buildings-pictures/mine.png"),
-  }
-];
-
 function Buildings() {
   const [buildingsData, setBuildingsData] = useState<buildingsInterface[]>([]);
 
@@ -46,14 +19,13 @@ function Buildings() {
       <section className="buildingsContainer">
         <ul className="buildingList">
           {buildingsData.map((building) => {
-            console.log(building.id);
             return (
               <li className="oneBuilding" key={building.id}>
                 <div className="buildingTitle">
-                  <h4>{building.name}</h4>
+                  <h4 className="buildingTitle">{building.name}</h4>
                 </div>
                 <div className="buildingLevel">
-                  <p>Building Level: {building.level}</p>
+                  <p className="buildingLevel">Building Level: {building.level}</p>
                 </div>
                 <div className="mineralCost">
                   <p>Mineral Cost: {building.mineralCost}</p>
