460bdadf459c4eef90de7950cd17d39cd400f340
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-01-18 16:12:06 +0100

    add login service logic

---
+++ backend/src/services/loginService.ts
@@ -0,0 +1,44 @@
+import User from '../models/user';
+import * as loginRepo from '../repositories/loginRepo';
+import { LoginResponse } from '../interfaces/login';
+import { NotFoundError, ParameterError } from '../errors';
+import jwt from 'jsonwebtoken';
+
+export async function getTokenByUserName(
+  user: string,
+  password: string
+): Promise<LoginResponse> {
+  if (!user && !password) {
+    throw new ParameterError('All fields required');
+  } else if (!password) {
+    throw new ParameterError('Password is required');
+  } else if (!user) {
+    throw new ParameterError('Username required');
+  }
+  const loggednInUser = await loginRepo.getUserByUserName(user);
+
+  if (!loggednInUser) {
+    throw new NotFoundError('No such username...');
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    loggednInUser.password,
+    password
+  );
+
+  if(!isPasswordCorrect) {
+    throw new NotFoundError('Wrong username or password')
+  }
+
+  const payload = {
+    id: loggednInUser.id,
+  };
+
+  if (loggednInUser) {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '3000',
+    });
+
+    return { Token: token };
+  }
+}
