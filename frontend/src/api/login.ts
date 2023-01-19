c321c8f43c2e39254731029acb4e882e4bbcc5da
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-01-19 21:32:13 +0100

    add api file, with fetch

---
+++ frontend/src/api/login.ts
@@ -0,0 +1,22 @@
+import { loginRequest } from "../interfaces/loginInterface";
+
+export async function fetchLogin(data: loginRequest): Promise<void> {
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const dbResponse = await response.json();
+    if (!response.ok) {
+      throw new Error(dbResponse.message);
+    }
+    return dbResponse;
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+  }
+}
+
+export default fetchLogin;
