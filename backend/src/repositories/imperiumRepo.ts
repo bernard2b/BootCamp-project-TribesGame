dfe912173c93f678dbae5d351db6245b6e38ed29
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-01-19 13:50:37 +0100

    add final, hopefully

--- backend/src/repositories/imperiumRepo.ts
+++ backend/src/repositories/imperiumRepo.ts
@@ -2,11 +2,11 @@ import Imperium from '../models/imperium';
 
 export function getImperiumById(id: number): Promise<Imperium | null> {
   return Imperium.findByPk(id);
+}
 
 export function createImperium(
   name: string,
-  userId: number,
-
+  userId: number
 ): Promise<Imperium> {
-  return imperium.create({ name, userId });
+  return Imperium.create({ name, userId });
 }
