c95d978c72b3ccd42d0d39a3006fccf34a733a78
Márton Fehér <martin.lee.white@gmail.com>
2023-01-25 16:32:28 +0100

    add all buildings by imperiumId endpoint

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -18,6 +18,7 @@ router.use(express.json());
 router.get('/hello', helloController.getHelloWorld);
 router.get('/buildings', buildingsController.getAllBuildings);
 router.get('/buildings/:buildingId', buildingsController.getOneBuildingById);
+router.get('/imperia/:imperiumId/buildings', buildingsController.getAllBuildingsByImperiumId)
 router.get('/imperia/troops', troopsController.getAllTroops);
 router.get('/imperia/:imperiumId/troops',  troopsController.getAllTroopsByImperiumId);
 router.get('/imperia/map/', imperiaController.getAllImperia)
