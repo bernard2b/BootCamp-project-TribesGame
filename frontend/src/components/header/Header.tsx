cf6c5b4db8f4055c21d9ff2e6088cee1b8f0553f
maakhu <egri@me.com>
2023-01-30 14:54:08 +0100

    add missing links & new logo artwork

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,54 +1,58 @@
-import React, {useState, useEffect} from "react";
-import './Header.scss';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./Header.scss";
+import { Link, useNavigate } from "react-router-dom";
 import Menu from "../Menu/Menu";
 
-
-
-
 export default function Header() {
-  const [settingsButton, setSettingsButton] = useState('');
-  const [logoutButton, setLogoutButton] = useState('');
-
+  const settings = "/user";
+  const logout = "/logout"
 
   const userData = [
     {
       id: "1",
       name: "John",
       level: 1,
-      img: require("./images/alien.png")
-    }
-  ]
+      img: require("./images/astronaut.png"),
+    },
+  ];
 
+  // const click = link('/register')
 
-  useEffect(() => {
-    if (window.location.pathname === "/") {
-      setSettingsButton("Settings");
-      setLogoutButton("Logout");
-    } else {
-      setSettingsButton("Register");
-      setLogoutButton("Login");
-    }
-  }, []);
-
+  // useEffect(() => {
+  //   if (window.location.pathname === "/") {
+  //     setSettingsButton("Settings");
+  //     setButton1Link(settings);
+  //     setLogoutButton("Logout");
+  //     setButton2Link("logout");
+  //   } else {
+  //     setSettingsButton("Register");
+  //     setButton1Link(register);
+  //     setLogoutButton("Login");
+  //     setButton2Link(login);
+  //   }
+  // }, []);
 
   return (
     <div className="container">
       <div className="header">
         <div className="logo">
-          <h1 className="kingdomName"><a href="/">My Kingdom</a></h1>
+          <h1 className="kingdomName">
+            <a href="/">Strigops Tribes®</a>
+          </h1>
         </div>
-          <Menu />
+        <Menu />
         <div className="navigation">
-            <h3>{settingsButton}</h3>
-            <h3>{logoutButton}</h3>
+          <Link to={settings} className="link">
+            Settings
+          </Link>
+          <Link to={logout} className="link">
+            Logout
+          </Link>
           {userData.map((user) => {
-            return (
-              <img src={user.img}  className="userImage"/>
-            )
+            return <img src={user.img} className="userImage" />;
           })}
         </div>
-      </div> 
+      </div>
     </div>
-  )
+  );
 }
