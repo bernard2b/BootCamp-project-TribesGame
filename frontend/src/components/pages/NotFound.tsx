dbe2a0731711bef69197b4c9dc5ce7cb332cc80e
maakhu <egri@me.com>
2023-01-30 18:58:59 +0100

    add new font family

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -5,18 +5,9 @@ import "./NotFound.scss";
 export default function NotFound() {
   return (
     <div className="NotFound">
-      <div className="text">
-        <h2>Stop your spaceship, traveller!</h2>
-        <br />
-        <h3>This space station is under construction. You might want </h3>
-        <h3> to come back later, or travel in time to its completion.</h3>
-        <br />
-        <p>Until then, please follow our patrol ships </p>
-        <p> back to the closest space station:</p>
-        <br />
+  
         <div className="button">
           <Link to="/imperia/buildings">Back to the galaxy</Link>
-        </div>
       </div>
     </div>
   );
