be3d9e3904015bcc0b4063d351a0131146422319
maakhu <egri@me.com>
2023-02-01 09:53:12 +0100

    adjust sql scripts for 1 coordinate (instead of 2)

--- backend/src/scripts/2playersAllBuildingsAndTroops.sql
+++ backend/src/scripts/2playersAllBuildingsAndTroops.sql
@@ -12,8 +12,8 @@ ALTER TABLE `tribes`.`imperia`
 CHANGE COLUMN `createdAt` `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
 CHANGE COLUMN `updatedAt` `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;
 
-INSERT into tribes.imperia (name, coordinateX, coordinateY, userId)
-VALUES ("Imperium1", 1, 1, 1);
+INSERT into tribes.imperia (name, coordinates, userId)
+VALUES ("Imperium1", 1, 1);
 
 INSERT into tribes.imperia (name, coordinateX, coordinateY, userId)
 VALUES ("Imperium2", 100, 100, 2);
